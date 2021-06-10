20922dd70bf032049673aa04d809a0f9aca53859
IzaqueL95 <izaque_14@live.com>
2021-06-10 20:53:26 -0300

    arquivo teste

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+
+
+function validaNome() {
+   
+    let testeNome = document.querySelector('.Title')  
+    let testa = document.getElementById('#T')
+   
+        Title.style.color = 'blue'
+    
+
+    
+    } 
