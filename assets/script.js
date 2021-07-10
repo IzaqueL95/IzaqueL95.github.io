d906e4e9c18c441d413e0e68cc427d32b3724d6a
IzaqueL95 <izaque_14@live.com>
2021-07-09 22:11:40 -0300

    Alterações visuais e implementação parcial do dark/white modo

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,39 @@
+const botao = document.querySelector('.botao')
+const menu = document.querySelector('.header')
+const botaoDark = document.querySelector('.botaodark')
+const botaoTeste = document.querySelector('.botaoT')
+const a = document.querySelector('.a')
+const body = document.querySelector('.bodyControl')
+const a1 = document.querySelector('.a1')
+const a2 = document.querySelector('.a2')
+const h1 = document.querySelector('.h1')
 
 
-function validaNome() {
-   
-    let testeNome = document.querySelector('.Title')  
-    let testa = document.getElementById('#T')
-   
-        Title.style.color = 'blue'
+botao.addEventListener("click", LightModo)
+function LightModo() {
+    h1.classList.add("active")
+    a.classList.add("active")
+    menu.classList.add("active")
+    a1.classList.add("active")
+    a2.classList.add("active")
+    body.classList.add("active")
+}
+
+botaoDark.addEventListener("click", dark)
+
+function dark(){
+    menu.classList.remove("active")
+    a.classList.remove("active")
+    body.classList.remove("active")
+    a1.classList.remove("active")
+    a2.classList.remove("active")
+    h1.classList.remove("active")
+}
+
+
+botaoTeste.addEventListener("click", testa)
+function testa(){
+    menu.classList.remove("active")
     
-
+}
     
-    } 
