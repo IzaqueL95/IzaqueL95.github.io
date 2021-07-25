0d08de8be975024ec822f2b95ff4f789a2176576
IzaqueL95 <izaque_14@live.com>
2021-07-25 20:44:54 -0300

    enviando alterações no dark and white

--- assets/script.js
+++ assets/script.js
@@ -7,27 +7,36 @@ const body = document.querySelector('.bodyControl')
 const a1 = document.querySelector('.a1')
 const a2 = document.querySelector('.a2')
 const h1 = document.querySelector('.h1')
+const h2 = document.querySelector('.h2')
+const h3 = document.querySelector('.h3')
+const eg = document.querySelector('.clasEgi')
 
 
-botao.addEventListener("click", LightModo)
-function LightModo() {
+botao.addEventListener("click", LightModo =>{
+    h2.classList.add("active")
     h1.classList.add("active")
-    a.classList.add("active")
-    menu.classList.add("active")
-    a1.classList.add("active")
-    a2.classList.add("active")
+    h3.classList.add("active")
+    a.classList.add("activeMenuItem")
+    menu.classList.add("activeMenu")
+    a1.classList.add("activeMenuItem")
+    a2.classList.add("activeMenuItem")
     body.classList.add("active")
-}
+    eg.classList.add("active")
+})
+
 
 botaoDark.addEventListener("click", dark)
 
 function dark(){
-    menu.classList.remove("active")
-    a.classList.remove("active")
+    menu.classList.remove("activeMenu")
+    a.classList.remove("activeMenuItem")
     body.classList.remove("active")
-    a1.classList.remove("active")
-    a2.classList.remove("active")
+    a1.classList.remove("activeMenuItem")
+    a2.classList.remove("activeMenuItem")
     h1.classList.remove("active")
+    h2.classList.remove("active")
+    h3.classList.remove("active")
+    eg.classList.remove("active")
 }
 
 
