1881043bac8a868da7ba8c9a1c60aa2ee260125f
Izaque Liborio <izaque_14@live.com>
2021-12-25 20:09:09 -0300

    atualização

--- scriptr.js
+++ scriptr.js
@@ -2,7 +2,8 @@ let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcbrl@trade') //usdt b
 let stockPrice = document.querySelector('#stock-price')
 let stockPriceCurrency = document.querySelector('#stock-currency')
 let howManyInner = document.querySelector('#how-many')
-
+let porc = document.querySelector('#porcentagem')
+let btcPerc = document.querySelector('#btc-perc')
 ws.onmessage = (event) => {
    let stockObjet = JSON.parse(event.data)
    
@@ -14,10 +15,29 @@ ws.onmessage = (event) => {
   
     stockPriceCurrency.innerText = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(stockObjet.p * 1509) 
 
-    howMany = 400000000 
+    var howMany = 400000000 
     var howIs = howMany /  stockObjet.p 
 
-    howManyInner.innerText = howIs.toFixed(2)
+    var perc = stockObjet.p * 1509
 
+     var resultsPerc = (perc / howMany -1) * 100
+
+     porc.innerText = '+' +  resultsPerc.toFixed(2) + ' %'
+      // console.log(perc)
+     if(perc > howMany){
+       porc.classList.add("colorG")
+     }
+
+     
+     howManyInner.innerText = howIs.toFixed(2)
+
+     var results = 1509
+     var btcResultPerc = (howIs / results - 1) * 100 
+     
+     btcPerc.innerText = btcResultPerc.toFixed(2) + ' %'
+
+     if(btcResultPerc < results){
+       btcPerc.classList.add("colorR")
+     }
   //   console.log(howIs.toFixed(2))
 }
