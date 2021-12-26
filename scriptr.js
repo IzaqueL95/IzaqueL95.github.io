55844b4b55711f6e419db7e8d87098446b773219
Izaque Liborio <izaque_14@live.com>
2021-12-25 22:56:32 -0300

    alterações no layout do código

--- scriptr.js
+++ scriptr.js
@@ -4,40 +4,41 @@ let stockPriceCurrency = document.querySelector('#stock-currency')
 let howManyInner = document.querySelector('#how-many')
 let porc = document.querySelector('#porcentagem')
 let btcPerc = document.querySelector('#btc-perc')
+
+var howMany = 400000000
+
 ws.onmessage = (event) => {
-   let stockObjet = JSON.parse(event.data)
-   
-   var formatado = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(stockObjet.p) 
+    let stockObjet = JSON.parse(event.data)
+
+    var formatado = Intl.NumberFormat('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(stockObjet.p)
 
     stockPrice.innerText = formatado
-    
-  //   formatado * 1522
-  
-    stockPriceCurrency.innerText = Intl.NumberFormat('pt-br', {style: 'currency', currency: 'BRL'}).format(stockObjet.p * 1509) 
 
-    var howMany = 400000000 
-    var howIs = howMany /  stockObjet.p 
+    stockPriceCurrency.innerText = Intl.NumberFormat('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(stockObjet.p * 1509)
+
+    var howIs = howMany / stockObjet.p
 
     var perc = stockObjet.p * 1509
 
-     var resultsPerc = (perc / howMany -1) * 100
+    var resultsPerc = (perc / howMany - 1) * 100
 
-     porc.innerText = '+' +  resultsPerc.toFixed(2) + ' %'
-      // console.log(perc)
-     if(perc > howMany){
-       porc.classList.add("colorG")
-     }
+    porc.innerText = '+' + resultsPerc.toFixed(2) + ' %'
 
-     
-     howManyInner.innerText = howIs.toFixed(2)
 
-     var results = 1509
-     var btcResultPerc = (howIs / results - 1) * 100 
-     
-     btcPerc.innerText = btcResultPerc.toFixed(2) + ' %'
+    howManyInner.innerText = howIs.toFixed(2)
 
-     if(btcResultPerc < results){
-       btcPerc.classList.add("colorR")
-     }
-  //   console.log(howIs.toFixed(2))
+    var results = 1509
+    var btcResultPerc = (howIs / results - 1) * 100
+
+    btcPerc.innerText = btcResultPerc.toFixed(2) + ' %'
+
+    perc > howMany ? porc.classList.add("colorG") : false
+    btcResultPerc < results ? btcPerc.classList.add("colorR") : false
+
 }
